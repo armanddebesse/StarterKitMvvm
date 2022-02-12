3b7bdac08eec6d5a5f3864be6e608048e6308733
Armand DEBESSE <armand.debesse@viacesi.fr>
2022-02-12 15:20:02 +0100

    [Add] Example for ErrorInfoTextBox

--- ExampleProject_Using_StarterKitMvvm/App.xaml.cs
+++ ExampleProject_Using_StarterKitMvvm/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExampleProject_Using_StarterKitMvvm.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,14 @@ namespace ExampleProject_Using_StarterKitMvvm
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new ViewModel()
+            };
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ ExampleProject_Using_StarterKitMvvm/ViewModels/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using StarterKitMvvmWpf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleProject_Using_StarterKitMvvm.ViewModels
+{
+    public class ViewModel : BaseViewModel
+    {
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                ClearErrors(nameof(Text));
+                if (_text == "Error")
+                {
+                    AddError(nameof(Text), "This is an error !");
+                }
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+    }
+}
